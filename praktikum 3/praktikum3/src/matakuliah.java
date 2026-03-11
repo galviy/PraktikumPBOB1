edcd03764e7da3844a4e87b89865dbc7caa5d1e2
galviy <galvindoser@gmail.com>
2026-03-11 13:06:27 +0700

    fuck this git

---
+++ praktikum 3/praktikum3/src/matakuliah.java
@@ -0,0 +1,57 @@
+public class matakuliah {
+
+    /**************** ATRIBUT ******************/
+    private String idMatkul, nama;
+    private int sks;
+
+
+    /**************** KONSTRUKTOR ******************/
+
+    // Konstruktor untuk membuat objek mata kuliah
+    public matakuliah(String idMatkul, String nama, int sks) {
+        this.idMatkul = idMatkul;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    // Konstruktor default
+    public matakuliah() {
+        this("", "", 0);
+    }
+
+
+    /**************** SELEKTOR ******************/
+
+    // Mengembalikan id mata kuliah
+    public String getIdMatkul() {
+        return this.idMatkul;
+    }
+
+    // Mengembalikan nama mata kuliah
+    public String getNama() {
+        return this.nama;
+    }
+
+    // Mengembalikan jumlah SKS mata kuliah
+    public int getSks() {
+        return this.sks;
+    }
+
+
+    /********************************/
+
+    // Mengubah id mata kuliah
+    public void setIdMatkul(String newIdMatkul) {
+        this.idMatkul = newIdMatkul;
+    }
+
+    // Mengubah nama mata kuliah
+    public void setNama(String newNama) {
+        this.nama = newNama;
+    }
+
+    // Mengubah jumlah SKS mata kuliah
+    public void setSks(int newSks) {
+        this.sks = newSks;
+    }
+}
